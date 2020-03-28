e6a195284e3955c9285ab51d9a96fd1d99b8b520
Nagnogur <trostinetsky.sasha1@gmail.com>
2020-03-28 18:35:24 +0200

    Added finding of the whole number

--- Lab_3/Program.cs
+++ Lab_3/Program.cs
@@ -10,25 +10,57 @@ namespace Lab_3
     {
         static void Main(string[] args)
         {
-            Tuple<char, int, string>[] operators = new Tuple<char, int, string>[5];
-            operators[0] = Tuple.Create('+', 2, "left");
-            operators[1] = Tuple.Create('-', 2, "left");
-            operators[2] = Tuple.Create('*', 3, "left");
-            operators[3] = Tuple.Create('/', 3, "left");
-            operators[4] = Tuple.Create('^', 4, "right");
+            List<Tuple<char, int, string> > operators = new List<Tuple<char, int, string> >();
+            char[] opers = {'+', '-', '*', '/', '^'};
+            char[] nums = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            operators.Add(Tuple.Create('+', 2, "left"));
+            operators.Add(Tuple.Create('-', 2, "left"));
+            operators.Add(Tuple.Create('*', 3, "left"));
+            operators.Add(Tuple.Create('/', 3, "left"));
+            operators.Add(Tuple.Create('^', 4, "right"));
 
             string s = "";
             Stack<string> output = new Stack<string>();
             Stack<char> operatorStack = new Stack<char>();
 
             string input = Console.ReadLine().Replace(" ", "");
-            Console.WriteLine(input);
-            Console.ReadKey();
+            int length = input.Length;
 
-            for (int i = 0; i < input.Length; i++)
+            for (int i = 0; i < length; i++)
             {
+                if (input[i] == '(')  //   (
+                {
 
+                }
+                
+                else if (input[i] == ')')   //      )
+                {
+
+                }
+
+                else if (opers.Contains(input[i]))      //      +,-,*,/,^
+                {
+
+                }
+                
+                else if (nums.Contains(input[i]))      //          numbers
+                {
+                    s += input[i];
+                    if (i != length && nums.Contains(input[i + 1]))
+                    {
+                        i++;
+                        while(i < length && nums.Contains(input[i]))
+                        {
+                            s += input[i];
+                            i++;
+                        }
+                        i--;
+                    }
+                    Console.WriteLine(s);
+                    s = "";
+                }
             }
+            Console.ReadKey();
         }
     }
 }
